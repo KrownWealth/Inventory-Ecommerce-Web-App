b874ca38321bd9d9dceb8f8c506b7ca3059d4b5a
KrownWealth <abdulramonadeola36@gmail.com>
2024-12-23 07:35:11 +0100

    Fixed cors error on production

--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,69 @@
 import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
+
+const applyCors = (req: NextRequestWithAuth) => {
+  const allowedOrigin = process.env.NEXT_PUBLIC_BASE_URL; 
+  const response = NextResponse.next();
+
+  const origin = req.headers.get("origin");
+  if (origin === allowedOrigin) {
+    response.headers.set("Access-Control-Allow-Origin", allowedOrigin);
+  }
+  response.headers.set("Access-Control-Allow-Credentials", "true");
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Content-Type"
+  );
+
+  if (req.method === "OPTIONS") {
+    return NextResponse.json(null, { status: 200, headers: response.headers });
+  }
+
+  return response; 
+};
+
 export default withAuth(
   function middleware(request: NextRequestWithAuth) {
+    // Apply CORS
+    const corsResponse = applyCors(request);
+    if (corsResponse) return corsResponse; 
+
     const { pathname } = request.nextUrl;
 
-    if(!request.nextauth.token){
-      return NextResponse.redirect(new URL('/auth/sign-in', request.url) )
+    // Authentication and authorization logic
+    if (!request.nextauth.token) {
+      return NextResponse.redirect(new URL("/auth/sign-in", request.url));
     }
 
-    // Admin route protection
-    if (pathname.startsWith('/dashboard')) {
-      if (!!request.nextauth.token && request.nextauth.token.role !== 'ADMIN') {
-        return NextResponse.redirect(new URL('/frontend', request.url)); 
+    if (pathname.startsWith("/dashboard")) {
+      if (!!request.nextauth.token && request.nextauth.token.role !== "ADMIN") {
+        return NextResponse.redirect(new URL("/frontend", request.url));
       }
     }
 
-    // User route protection
-    if (pathname.startsWith('/frontend')) {
-      if (!!request.nextauth.token && request.nextauth.token.role !== 'NORMAL_USER') {
-        return NextResponse.redirect(new URL('/dashboard', request.url)); 
+    if (pathname.startsWith("/frontend")) {
+      if (!!request.nextauth.token && request.nextauth.token.role !== "NORMAL_USER") {
+        return NextResponse.redirect(new URL("/dashboard", request.url));
       }
     }
 
-    return NextResponse.next(); 
+    return NextResponse.next(); // Continue to the requested route
   },
-  
   {
     callbacks: {
-      authorized: ({ token }) => !!token, // allow user if token exist
+      authorized: ({ token }) => !!token,
     },
   }
 );
 
 export const config = {
   matcher: [
-    '/dashboard/:path*',
-    '/frontend/:path*',  
+    "/dashboard/:path*",
+    "/frontend/:path*",
   ],
 };
